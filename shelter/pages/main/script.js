c0032527feda185c24b81030a01db7525690eac1
Yury Ahanouski <uraaru@live.com>
2022-04-13 19:44:03 +0300

    fix: circle btn on pets page is non-interactive now

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,3 +1,3 @@
-alert(
-  "Hi, thanks for waiting) The only thing that may cause you confusion is the pictures in our-friends section. Yes, they are different and of better quality. Downloaded from discord, so do not take points off, please"
-);
+// alert(
+//   "Hi, thanks for waiting) The only thing that may cause you confusion is the pictures in our-friends section. Yes, they are different and of better quality. Downloaded from discord, so do not take points off, please"
+// );
