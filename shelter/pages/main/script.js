4dc27d438834052cbb8fb4886946f0eb5a52925a
Yury Ahanouski <uraaru@live.com>
2022-04-19 01:17:18 +0300

    feat: add adaptive our-friends section on pets page

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,3 +1,1 @@
-// alert(
-//   "Hi, thanks for waiting) The only thing that may cause you confusion is the pictures in our-friends section. Yes, they are different and of better quality. Downloaded from discord, so do not take points off, please"
-// );
+// alert("Hi, please? give me maybe a half of day. There's very little left");
